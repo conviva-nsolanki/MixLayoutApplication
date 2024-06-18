74c0f90b0a5d349a09db4be7eabc4f1873f48e9d
nsolanki <nsolanki@conviva.com>
2024-06-18 15:26:10 -0700

    implement custom modifier and custom button to simplify the custom click event

--- app/src/main/java/com/example/mixlayoutapplication/ui/ComposeContent.kt
+++ app/src/main/java/com/example/mixlayoutapplication/ui/ComposeContent.kt
@@ -1,13 +1,16 @@
 package com.example.mixlayoutapplication.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +19,9 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -32,8 +38,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -172,18 +180,14 @@ fun ParkContentPreview() {
 fun ParkActionRwoItems(modifier: Modifier, name: String, onClick: (String) -> Unit) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         // Visit park button
-        Button(
+        LogAnalyticsButton(
             modifier = modifier
                 .padding(start = 8.dp, end = 8.dp)
                 .width(150.dp),
             onClick = {
                 onClick("Visit ${name}")
-                ConvivaAppAnalytics
-                    .getDefaultTracker()
-                    ?.trackCustomEvent("button_click", JSONObject().also {
-                        it.put("Park name", name)
-                    })
-            }) {
+            },
+            eventValue = name) {
             Text(text = "Visit")
         }
         // Navigation Image
@@ -192,13 +196,8 @@ fun ParkActionRwoItems(modifier: Modifier, name: String, onClick: (String) -> Un
                 .height(IntrinsicSize.Min) // Match height to Button
                 .width(IntrinsicSize.Min)
                 .padding(top = 8.dp, bottom = 8.dp, start = 4.dp, end = 4.dp)
-                .clickable {
+                .logEventClickable(name) {
                     onClick("Navigate ${name}")
-                    ConvivaAppAnalytics
-                        .getDefaultTracker()
-                        ?.trackCustomEvent("imageview_click", JSONObject().also {
-                            it.put("Park name", name)
-                        })
                 },
             contentScale = ContentScale.Fit,
             painter = painterResource(R.drawable.baseline_assistant_navigation_24),
@@ -237,6 +236,72 @@ fun ParkImage(modifier: Modifier, imageUrl: String) {
             .fillMaxWidth()
             .height(250.dp),
         contentScale = ContentScale.Crop // Adjust scaling as needed
+    )
+}
+
+/**
+ * create a custom clickable modifier using existing
+ * [clickable] modifier that can be reused
+ */
+@Composable
+fun Modifier.logEventClickable(
+    eventValue: String,
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit
+): Modifier = this.clickable(
+    enabled,
+    onClickLabel,
+    role,
+    onClick = {
+        // Log event
+        ConvivaAppAnalytics
+            .getDefaultTracker()
+            ?.trackCustomEvent("button_click", JSONObject().also {
+                it.put("Park name", eventValue)
+            })
+        onClick.invoke()
+    }
+)
+
+
+/**
+ * create a custom button that log event when click action happens
+ */
+@Composable
+fun LogAnalyticsButton(
+    eventValue: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        onClick = {
+            // Log event
+            ConvivaAppAnalytics
+                .getDefaultTracker()
+                ?.trackCustomEvent("button_click", JSONObject().also {
+                    it.put("Park name", eventValue)
+                })
+            onClick.invoke()
+        },
+        modifier,
+        enabled,
+        shape,
+        colors,
+        elevation,
+        border,
+        contentPadding,
+        interactionSource,
+        content
     )
 }
 
